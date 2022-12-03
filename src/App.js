da115b798762bcc3288a8889a083c6c2026a958a
Aleksandr Vasiljev <zaswet@gmail.com>
2022-12-03 17:40:00 +0200

    topbanner component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import TopBanner from './components/TopBanner/TopBanner';
 
 function App() {
   return (
     <>
-    
+      <div>
+        <TopBanner></TopBanner>
+      </div>
     </>
   );
 }
---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,29 @@
+import React, { Component, Fragment } from 'react'
+import { Col, Container, Row, Button } from 'react-bootstrap'
+import '../../asset/css/custom.css';
+import '../../asset/css/bootstrap.min.css';
+
+export class TopBanner extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container fluid={true} className="topFixedBanner p-0">
+          <div className="topBannerOverlay">
+            <Container className="topContent">
+              <Row>
+                <Col className="text-center">
+                  <h1 className="topTitle">EASY LEARNING</h1>
+                  <h4 className="topSubTitle">Learn Professionally</h4>
+                  <Button variant="primary">Learn More</Button>
+                </Col>
+              </Row>
+            </Container>
+
+          </div>
+        </Container>
+      </Fragment>
+    )
+  }
+}
+
+export default TopBanner
