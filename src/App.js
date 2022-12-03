6f58482513ae03e1183b30cf7c2da5a6ce99165d
Aleksandr Vasiljev <zaswet@gmail.com>
2022-12-03 20:28:53 +0200

    topNavigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Services from './components/Services/Services';
 import TopBanner from './components/TopBanner/TopBanner';
+import TopNavigation from './components/TopNavigation/TopNavigation';
 
 function App() {
   return (
     <>
       <div>
+        <TopNavigation></TopNavigation>
         <TopBanner></TopBanner>
         <Services></Services>
       </div>
---
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -0,0 +1,74 @@
+import React, { Component, Fragment } from 'react'
+import '../../asset/css/custom.css';
+import '../../asset/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import whiteLogo from '../../asset/image/logo_white.png'
+import blackLogo from '../../asset/image/logo_black.png'
+
+export class TopNavigation extends Component {
+
+    constructor()
+    {
+        super();
+        this.state={
+            navBarTitle: "navTitle",
+            navBarLogo: [whiteLogo], //obj
+            navBarBack: "navBackground",
+            navBarItem: "navItem"
+        }
+    }
+
+    onScroll = () => {
+        if(window.scrollY > 469) { //set to 800 later
+            this.setState({
+                navBarTitle: 'navTitleScroll', 
+                navBarLogo: [blackLogo], 
+                navBarBack: 'navBackgroundScroll',
+                navBarItem: 'navItemScroll'
+            })
+        }
+        else if(window.scrollY < 469){ //set to 500 later
+            this.setState({
+                navBarTitle: 'navTitle',
+                navBarLogo: [whiteLogo],
+                navBarBack: 'navBackground',
+                navBarItem: 'navItem'
+            })
+        }
+    }
+
+    componentDidMount()
+    {
+        window.addEventListener('scroll', this.onScroll)
+    }
+
+
+
+    render() {
+        return (
+            <Fragment>
+                <Navbar className={this.state.navBarBack} collapseOnSelect fixed="top" expand="lg" variant="dark">
+                        <Navbar.Brand className={this.state.navBarTitle}  href="#home">
+                            <img src={this.state.navBarLogo} />
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto"></Nav>
+                            <Nav style={{ 
+                                marginRight: '20px'
+                             }}>
+                                <Nav.Link className={this.state.navBarItem} href="#deets">HOME</Nav.Link>
+                                <Nav.Link className={this.state.navBarItem} href="#deets">ABOUT</Nav.Link>
+                                <Nav.Link className={this.state.navBarItem} href="#deets">SERVICE</Nav.Link>
+                                <Nav.Link className={this.state.navBarItem} href="#deets">COURSES</Nav.Link>
+                                <Nav.Link className={this.state.navBarItem} href="#deets">PORTFOLIO</Nav.Link>
+                                <Nav.Link className={this.state.navBarItem} href="#deets">CONTACT US</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                </Navbar>
+            </Fragment>
+        )
+    }
+}
+
+export default TopNavigation
