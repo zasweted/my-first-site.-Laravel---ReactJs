534ca93ed82bf03b91bea0de64489935c8991889
Aleksandr Vasiljev <zaswet@gmail.com>
2022-12-20 19:47:40 +0200

    courses component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Analysis from './components/Analysis/Analysis';
+import Courses from './components/Courses/Courses';
 import RecentProject from './components/RecentProject/RecentProject';
 import Services from './components/Services/Services';
 import Summary from './components/Summary/Summary';
@@ -16,6 +17,7 @@ function App() {
         <Analysis></Analysis>
         <Summary></Summary>
         <RecentProject></RecentProject>
+        <Courses></Courses>
       </div>
     </>
   );
