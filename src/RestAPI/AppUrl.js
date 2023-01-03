5f880fab8ed4897c498dcaac2c44d2446ab07680
Aleksandr Vasiljev <zaswet@gmail.com>
2023-01-03 20:53:52 +0200

    appurl

---
+++ src/RestAPI/AppUrl.js
@@ -0,0 +1,34 @@
+
+
+class AppUrl
+{
+    static BaseURL = 'http://127.0.0.1:8000/api';
+    
+    static HomeTechDescription = this.BaseURL + '/home/techDescription';
+    static HomePageTitle = this.BaseURL + '/home/pageTitle';
+    static HomeTotalStats = this.BaseURL + '/home/totalStats';
+    static HomeVideoData = this.BaseURL + '/home/videoData';
+
+    static ProjectsDetails = this.BaseURL + '/projectsDetails';
+    static ProjectsHomePageData = this.BaseURL + '/projectsHomePageData';
+    static ProjectsDataAll = this.BaseURL + '/projectsDataAll';
+
+    static ServicesData = this.BaseURL + '/servicesData';
+
+    static InformationData = this.BaseURL + '/informationData';
+
+    static FooterData = this.BaseURL + '/footerData';
+
+    static CoursesDetails = this.BaseURL + '/coursesDetails';
+    static CoursesHomePageDdata = this.BaseURL + '/coursesHomePageData';
+    static CoursesDataAll = this.BaseURL + '/coursesDataAll';
+
+    static ContactsStore = this.BaseURL + '/contactsStore';
+
+    static ClientReviewData = this.BaseURL + '/clientReviewData';
+
+    static TechChartData = this.BaseURL + '/chartData';
+
+} 
+
+export default AppUrl
