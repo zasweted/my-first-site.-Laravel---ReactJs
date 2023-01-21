9675c22e01ae28d77f1ae99caeaa7ddeac83d621
Aleksandr Vasiljev <zaswet@gmail.com>
2023-01-21 15:28:11 +0200

    courses api consume + courses card component

---
+++ src/components/Courses/CoursesCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+function CoursesCard ({data}) {
+    console.log(data);
+    return(
+      <>
+          {
+              data.map(data => {
+                   return <Col lg={6} md={12} sm={12}>
+                      <Row>
+                          <Col className="p-2" lg={6} md={6} sm={12}>
+                              <img className="courseImage" src={data.short_image} />
+                          </Col>
+      
+                          <Col lg={6} md={6} sm={12}>
+                              <h5 className="serviceName text-start">{data.short_title}</h5>
+                              <p className="serviceDescription text-start">{data.short_description}</p>
+                              <Link className="float-start courseViewMore" to="/course-details" >View Details</Link>
+                          </Col>
+                      </Row>
+      
+                  </Col>
+              })
+          }
+      </>
+    )
+}
+
+
+export default CoursesCard
